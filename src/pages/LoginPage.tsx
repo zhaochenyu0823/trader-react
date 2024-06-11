91da903323ca92ce20cc5a836b77e09683a6b069
WEL-ADNET\C_ZHAO <Chenyu_Zhao-p@wellnet.co.jp>
2024-06-11 15:21:51 +0900

    sing in sing out 追加

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
-import { SignInOutput, signIn, signOut } from 'aws-amplify/auth';
-import SignOutButton from './SignOutButton';
+import { SignInOutput, getCurrentUser, signIn, signOut } from 'aws-amplify/auth';
+import { fetchAuthSession } from 'aws-amplify/auth';
 
 
 
@@ -11,16 +11,35 @@ const LoginPage = () => {
   const [challengeResponse, setChallengeResponse] = useState('');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [getuser, setUser] = useState<SignInOutput | null>(null);
-  const [stage, setStage] = useState(1);
+  const [stage] = useState(1);
+
+  // const fetchTokens = async () => {
+  //   try {
+  //     const { tokens } = await fetchAuthSession({ forceRefresh: true });
+  //     console.log(tokens);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
 
 
-
+  async function currentAuthenticatedUser() {
+    try {
+      const { username, userId, signInDetails } = await getCurrentUser();
+      console.log(`The username: ${username}`);
+      console.log(`The userId: ${userId}`);
+      console.log(`The signInDetails: ${signInDetails}`);
+      setIsLoggedIn(true);
+    } catch (err) {
+      setIsLoggedIn(false);
+      console.log(err);
+    }
+  }
 
   useEffect(() => {
-    // 恢复登录状态和用户信息
-    if (getuser) {
-      setIsLoggedIn(true);
-    }
+
+    currentAuthenticatedUser();
+
   }, []);
 
 
@@ -40,14 +59,19 @@ const LoginPage = () => {
           //setStage(2);
           console.log("signInStep" + user.nextStep.signInStep);
         })
-
-      //   if (user.nextStep.signInStep === 'CUSTOM_CHALLENGE') {
-      //     setStage(2);
-      //   }
     } catch (error) {
       console.log('Error signing in', error);
     }
   };
+
+  async function handleSignOut() {
+    try {
+      await signOut();
+      setIsLoggedIn(false);
+    } catch (error) {
+      console.log('error signing out: ', error);
+    }
+  }
 
 
   const handleSubmitChallenge = async () => {
@@ -62,28 +86,16 @@ const LoginPage = () => {
     // }
   };
 
-  async function handleSignOut() {
-    try {
-      await signOut();
-      setIsLoggedIn(false);
-    } catch (error) {
-      console.log('error signing out: ', error);
-    }
-  }
-
-
-  
-
-
-
   return (
 
-
-
-    
     <div>
-      <button onClick={handleSignOut}>Sign out</button>
-      {stage === 1 && (
+      {isLoggedIn ? (
+        <div>
+          <p>Hello </p>
+          <button onClick={handleSignOut}>Sign out</button>
+        </div>
+
+      ) : (
         <div>
           <input
             type="text"
@@ -100,41 +112,7 @@ const LoginPage = () => {
           <button onClick={handleSignIn}>Sign In</button>
         </div>
       )}
-
-      {stage === 2 && (
-        <div>
-          <input
-            type="text"
-            placeholder="Enter verification code"
-            value={challengeResponse}
-            onChange={(e) => setChallengeResponse(e.target.value)}
-          />
-          <button onClick={handleSubmitChallenge}>Submit</button>
-        </div>
-      )}
-
-
-
-
-
-
-
-
-      {isLoggedIn ? (
-        <div>
-          <p>Hello </p>
-          <button onClick={handleSignOut}>Sign out</button>
-        </div>
-
-      ) : (
-        <p>Please sign in.</p>
-      )}
-
-      <SignOutButton/>
-    
-
     </div>
-
   );
 
 };
